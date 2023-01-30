22958c710308a4e2dfa0d9de86ef0a75e7037b6f
ninadth <ninadthakre010@gmail.com>
2023-01-30 18:06:20 +0530

    changes by ninad

---
+++ src/main/java/com/BikkadIT/DI/GittutApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.DI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittutApplication.class, args);
+	}
+
+}
